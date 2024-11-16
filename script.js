994059fb17cc18d102f77365b55a090ca35381ff
bbantier <65014410+bbantier@users.noreply.github.com>
2024-11-16 10:21:01 +0100

    create DOM elements in JS and append them

---
+++ script.js
@@ -0,0 +1,24 @@
+const container = document.querySelector("#container");
+const redParagraph = document.createElement("p");
+const blueTitle = document.createElement("h3");
+const pinkDiv = document.createElement("div");
+const divTitle = document.createElement("h1");
+const divParagraph = document.createElement("p");
+
+redParagraph.style.color = "red";
+redParagraph.textContent = "Hey, I'm red!";
+
+blueTitle.style.color = "blue";
+blueTitle.textContent = "I'm a blue H3!";
+
+pinkDiv.style.cssText = "border, 1px solid black; backgroundColor, pink";
+
+divTitle.textContent = "I'm in a div";
+divParagraph.textContent = "ME TOO!";
+
+pinkDiv.appendChild(divTitle);
+pinkDiv.appendChild(divParagraph);
+
+container.appendChild(redParagraph);
+container.appendChild(blueTitle);
+container.appendChild(pinkDiv);
